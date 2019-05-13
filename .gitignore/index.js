4932ec9dac591eb3eebdbd70824e61ce8870b132
Poul0s <43917783+Poul0s@users.noreply.github.com>
2019-05-13 21:52:13 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -297,7 +297,7 @@ botcyb.on("message", message => {
         })
             return;
        }
-       if(message.content("<@293857217365540895>")){
+       if(message.content.startsWith("<@293857217365540895>")){
            var auth = message.author
         message.guild.channels.find("name", "💪salon-staff💪").send(`${auth} a mentionné cyber ${message.channel.name} <@519186194886688779>`)
        }
