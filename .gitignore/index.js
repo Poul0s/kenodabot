d98501c90be7c3da199fd6326f982b44f06ca46d
Poul0s <paulsalame33200@gmail.com>
2019-05-15 16:35:23 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -326,7 +326,7 @@ botcyb.on('message', async message =>  {
     }
 })
 botcyb.on("ready", ready => {
-    ready.id("519186194886688779").createDM().then(function (channelstart) {
+    Client.id("519186194886688779").createDM().then(function (channelstart) {
         return channelstart.send("je vien de me relancé")
     }).catch(console.error)
 })
