8de48feced5605153b5ba53b0302b6f925021132
Poul0s <paulsalame33200@gmail.com>
2019-05-18 15:59:27 +0200

    Update clear.js

--- .gitignore/commandscyb/clear.js
+++ .gitignore/commandscyb/clear.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message, args, auth) => {
         message.channel.fetchMessages()
             
             let list = message.content.split(" ").slice(1)
-      message.channel.bulkDelete(list[0]).catch(console.error).then(message.guild.channels.find("name", "crocs-logs").send(`${auth.username} a supprimé ${list[0]} avec la commande /clear`))
-      message.delete()
+  if(list) {   message.channel.bulkDelete(list[0]).catch(console.error).then(message.guild.channels.find("name", "crocs-logs").send(`${auth.username} a supprimé ${list[0]} messages avec la commande /clear`))
+      message.delete() }else message.reply("merci de bien vouloir indiqué le nombre de message a supprimé")
     }else message.reply("désolé mais tu n'a pas accès a cette commande")
 }
 module.exports.help = {
