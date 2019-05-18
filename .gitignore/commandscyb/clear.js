2f974400d8266e8e56c8a8b08fed05383ac1e725
Poul0s <paulsalame33200@gmail.com>
2019-05-18 15:49:04 +0200

    update index.js + opti

---
+++ .gitignore/commandscyb/clear.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+module.exports.run = async (bot, message, args, auth) => {
+    if(message.member.hasPermission("MANAGE_MESSAGES")){
+        message.channel.fetchMessages()
+            
+            let list = message.content.split(" ").slice(1)
+      message.channel.bulkDelete(list[0]).catch(console.error).then(message.guild.chanels.find("name", "crocs-logs").send(`{auth.username} a supprimé ${list[0]} avec la commande /clear`))
+      message.delete()
+    }else message.reply("désolé mais tu n'a pas accès a cette commande")
+}
+module.exports.help = {
+    name: "clear"
+}
--- .gitignore/index.js
+++ .gitignore/index.js
@@ -259,32 +259,11 @@ fs.readdir("./commandscyb/", (err, files) => {
 
 botcyb.on("message", message => {
 var auth = message.author
-    if(message.content.startsWith(prefixcyb + "report")){
-        const reportmember = message.mentions.members.first();
-        if (!reportmember) return message.reply("Tu n'as pas mentionner d'utilisateur.");
-        let args1 = message.content.split(" " + reportmember + " ").slice(1);
-        let args2 = args1
-        if(!args2) return message.reply("tu n'as pas mis de raison pour ton avertissement");
-        var auth = message.author
-        let reportembed = new Discord.RichEmbed()
-        .setTitle(`Report de ${auth}`)
-        .setColor("FFFF00")
-        .addField("Salon", `${message.channel.name}`, true)
-        .addField("Utilisateur", `${reportmember}`, true)
-        .addField("Raison", `${args2}`, true)
-        message.guild.channels.find("name", "crocs-logs").send(reportembed)
-        message.guild.channels.find("name", "crocs-logs").send("Mention <@519186194886688779>")
-  //      message.guild.channels.find("name", "crocs-logs").send(`${auth} a report ${reportmember} dans le channel ${message.channel.name}pour: ${args2} \n[mention: <@519186194886688779>]`)
-        message.channel.send("Merci pour ton report, n'oublie pas de screen si jamais un modérateur te le demande")
-        
-        return;
-    }
     if(message.content.startsWith(prefix + "clear")) {
         if(message.member.hasPermission("MANAGE_MESSAGES")){
             message.channel.fetchMessages()
                 
                 let list = message.content.split(" ").slice(1)
-//If(!list) return message.reply("tu n’a pas mit de nombre de message à supprimer")
           message.channel.bulkDelete(list[0]).catch(console.error)
           message.delete()
           message.channel.bulkDelete(list[0]).catch(console.error)
