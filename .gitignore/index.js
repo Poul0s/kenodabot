94d740ceaa9dda53bcd713f23075542a4f6af18e
Poul0s <paulsalame33200@gmail.com>
2019-05-18 16:40:01 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -261,8 +261,8 @@ fs.readdir("./commandscyb/", (err, files) => {
 botcyb.on("message", message => {
     var blackembed = new Discord.RichEmbed()
   .setColor("RANDOM")
-  .setTitle("Message impossible à effectuer!")
-  .addField("vous ne pouvez pas envoyé de message ici car vous n'êtes pas autorisé à utilisé le bot dans ce serveur là", true)
+  .setTitle("commande impossible à effectuer!")
+  .addField("vous ne pouvez pas exécuté de message ici car vous n'êtes pas autorisé à utilisé le bot dans ce serveur là, pour plsu d'information, veuillez contacté thunlos", true)
   .setImage("http://www.parisenligne.com/wp-content/imgs/image-liste-noire-70.jpg")
    if(message.content.startsWith(prefix)) {
     if(!message.guild.id === "517032764571385857") {
