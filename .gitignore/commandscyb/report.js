23948488de1769ef7a0b50f856f2664c4e87a32a
Poul0s <paulsalame33200@gmail.com>
2019-06-10 19:49:13 +0200

    fdfdgsdgsdfg

--- .gitignore/commandscyb/report.js
+++ .gitignore/commandscyb/report.js
@@ -8,17 +8,19 @@ module.exports.run = async (botcyb, message, args, auth) => {
     if(!args2) return message.reply("tu n'as pas mis de raison pour ton avertissement");
     var auth = message.author
     let reportembed = new Discord.RichEmbed()
-    .setTitle(`Report de ${auth}`)
+    .setTitle(`Report de <@${auth.id}>`)
     .setColor("FFFF00")
     .addField("Salon", `${message.channel.name}`, true)
     .addField("Utilisateur", `${reportmember}`, true)
     .addField("Raison", `͔${args2}`, true)
+    message.guild.channels.find("name", "crocs-logs").send("@[Modérateur]")
     message.guild.channels.find("name", "crocs-logs").send(reportembed)
 //      message.guild.channels.find("name", "crocs-logs").send(`${auth} a report ${reportmember} dans le channel ${message.channel.name}pour: ${args2} \n[mention: <@519186194886688779>]`)
-    message.channel.send("Merci pour ton report, n'oublie pas de screen si jamais un modérateur te le demande \nTout abus de report est sanctionnable")
+    message.channel.send("Votre report a été signalé à l'équipe de modération, n'oublie pas de screen si jamais un modérateur te le demande \nTout abus de report est sanctionnable")
     
     return;
 }
 module.exports.help = {
     name: "/report"
 }
+
