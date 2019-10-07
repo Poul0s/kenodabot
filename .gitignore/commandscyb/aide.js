3c90906b2b75fe8b28f8fd785cda18ded89a595c
Poul0s <43917783+Poul0s@users.noreply.github.com>
2019-10-07 11:02:04 +0200

    Update aide.js

--- .gitignore/commandscyb/aide.js
+++ .gitignore/commandscyb/aide.js
@@ -4,6 +4,16 @@ module.exports.run = async (botcyb, message, args, auth) => {
   
     auth.createDM().then(function (soon) {
      return soon.send("la commande /aide est en cours de développement par le plus beau des rebeu THUNLOS");
+        
+        /*
+        
+        
+        
+        
+        
+        
+        
+        */
     })
         return;
 }
