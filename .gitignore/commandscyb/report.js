7288d8d8c38fb3cfb6b383c47ded7f3713b625c7
Poul0s <paulsalame33200@gmail.com>
2019-12-23 14:24:55 +0100

    Update report.js

--- .gitignore/commandscyb/report.js
+++ .gitignore/commandscyb/report.js
@@ -14,10 +14,10 @@ module.exports.run = async (botcyb, message, args, auth) => {
     .addField("Utilisateur", `${reportmember}`, true)
     .addField("Raison", `${args11}`, true)
     message.guild.channels.find("name", "crocs-logs").send("<@&517053064792899584>")
-    message.guild.channels.find("name", "crocs-logs").send(reportembed).react(':white_check_mark:', ':negative_squared_cross_mark:')
+    var send = message.guild.channels.find("name", "crocs-logs").send(reportembed)
 //      message.guild.channels.find("name", "crocs-logs").send(`${auth} a report ${reportmember} dans le channel ${message.channel.name}pour: ${args11} \n[mention: <@519186194886688779>]`)
     message.channel.send("Votre report a été signalé à l'équipe de modération, n'oublie pas de screen si jamais un modérateur te le demande \nTout abus de report est sanctionnable")
-    
+    send.react(':white_check_mark:', ':negative_squared_cross_mark:')
     return;
 }
 module.exports.help = {
