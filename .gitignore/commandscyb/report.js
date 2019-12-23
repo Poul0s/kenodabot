729dc4d517aeaef1f62dcd9f9808296863e7a470
Poul0s <paulsalame33200@gmail.com>
2019-12-23 14:43:53 +0100

    Update report.js

--- .gitignore/commandscyb/report.js
+++ .gitignore/commandscyb/report.js
@@ -17,7 +17,7 @@ module.exports.run = async (botcyb, message, args, auth) => {
     var sen = message.guild.channels.find("name", "crocs-logs").send(reportembed)
 //      message.guild.channels.find("name", "crocs-logs").send(`${auth} a report ${reportmember} dans le channel ${message.channel.name}pour: ${args11} \n[mention: <@519186194886688779>]`)
     message.channel.send("Votre report a été signalé à l'équipe de modération, n'oublie pas de screen si jamais un modérateur te le demande \nTout abus de report est sanctionnable")
-    sen.react('\u2705')
+    sen.react("\u2705")
     sen.react("\u274e")
     return;
 }
