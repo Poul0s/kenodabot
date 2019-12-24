8fd29314aa624302e50c42eaddd7d84095db5cee
Poul0s <paulsalame33200@gmail.com>
2019-12-24 13:14:37 +0100

    Update aide.js

--- .gitignore/commandscyb/aide.js
+++ .gitignore/commandscyb/aide.js
@@ -10,14 +10,14 @@ module.exports.run = async (botcyb, message, args, auth) => {
         var exutil = ""
         var permission = "Tout le monde à accès à cette commande"
      switch (args) {
-            case '/ban' :
+            case ' /ban' :
            var desc = "La commande /ban permet d'interdire l'accès au serveur à un certain utilisateur pour une infraction qu'il a commise."
             var util = "La commande s'utilise avec le tag du membre à bannir suivi du temp (optionnel) en seconde(sec)/minute(min)/heure(h)/jour(d)/mois(m) et de la raison pour finir." 
             var exutil = "/ban <@276060004262477825> 10min mentheur (Koya sera banni durant 10 minute car c'est un mentheur. PS: la raison mentheur n'est pas une raison suffisante pour bannir un membre.) "
             var permission = "Utilisation seulement autorisé aux membres du staff ayant le grade modérateur ou plus."
             embred(desc, util, exutil, permission)
             break;
-            // default: auth.createDM().then(function (soo) { return soo.send("Utilise la commande /help pour voir la liste des commande que tu peux utilisé avec la commande /aide. Si vous trouvez une erreur, merci de contactez <@519186194886688779>") })
+            default: auth.createDM().then(function (soo) { return soo.send("Utilise la commande /help pour voir la liste des commande que tu peux utilisé avec la commande /aide. Si vous trouvez une erreur, merci de contactez <@519186194886688779>") })
         }
        function embred(desc, util, exutil, permission) {
      var embedaide = new Discord.RichEmbed()
