cc440a2c3f38137469f2b4ef7451600aaa9636e1
Poul0s <paulsalame33200@gmail.com>
2019-12-24 13:22:56 +0100

    Update aide.js

--- .gitignore/commandscyb/aide.js
+++ .gitignore/commandscyb/aide.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-module.exports.run = (botcyb, message, args, auth) => {
+module.exports.run = async (botcyb, message, args, auth) => {
     
     auth.createDM().then(function (soon) {
      return soon.send("la commande /aide est en cours de développement par le plus beau des rebeu THUNLOS");
